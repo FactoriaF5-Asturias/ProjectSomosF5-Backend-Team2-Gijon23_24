aedef2750f8b489a1dbc9ef76b7e40c4242b7f7d
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-03-05 14:34:25 +0100

    first commit

---
+++ src/main/java/org/teamraccoon/dreamfusion/DreamfusionApplication.java
@@ -0,0 +1,13 @@
+package org.teamraccoon.dreamfusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamfusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamfusionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/teamraccoon/dreamfusion/DreamfusionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teamraccoon.dreamfusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamfusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
