2f3629ae4ec2f846d42995153deafc60e9386886
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-03-11 14:13:21 +0100

    product model is ready

---
+++ src/main/java/org/teamraccoon/dreamfusion/products/Product.java
@@ -0,0 +1,40 @@
+package org.teamraccoon.dreamfusion.products;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "products")
+public class Product {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    @Column(name = "id_country")
+    private Long id;
+
+    @Column(name = "product_name")
+    private String productName;
+
+    @Column
+    private String description;
+
+    @Column(name = "product_image")
+    private String productImage;
+
+    @Column
+    private int price;
+}
