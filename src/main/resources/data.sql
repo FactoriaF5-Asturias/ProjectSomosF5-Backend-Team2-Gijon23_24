d2f6f8c0530595ada6259af2cfd154414833b1a1
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-03-12 15:52:09 +0100

    more products added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,7 +7,11 @@ INSERT INTO categories (id_category, category_name) VALUES (default, 'Litofonía
 /*Products*/
 
 INSERT INTO products (id_product, product_name, product_description, product_image, price) VALUES (default, 'Estatua de Batman', 'Una estatua de Batman en 3D', 'batman-statue.jpg', 29.99);
+INSERT INTO products (id_product, product_name, product_description, product_image, price) VALUES (default, 'Estatua de Superman', 'Una estatua de Superman en 3D', 'superman-statue.jpg', 29.99);
+INSERT INTO products (id_product, product_name, product_description, product_image, price) VALUES (default, 'Estatua de Wonder Woman', 'Una estatua de Wonder Woman en 3D', 'wonder-woman-statue.jpg', 29.99);
 
 /*Categories-Products*/
 
 INSERT INTO categories_products (product_id, category_id) VALUES (1, 2);
+INSERT INTO categories_products (product_id, category_id) VALUES (2, 2);
+INSERT INTO categories_products (product_id, category_id) VALUES (3, 2);
