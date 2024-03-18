0ef13c724b5db8be0d09d55be04e011db0adb519
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-03-18 11:50:10 +0100

    get call is working

---
+++ src/main/java/org/teamraccoon/dreamfusion/generic/IStorageService.java
@@ -0,0 +1,19 @@
+package org.teamraccoon.dreamfusion.generic;
+
+import java.nio.file.Path;
+
+import org.springframework.core.io.Resource;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public interface IStorageService {
+
+	Path load(String filename);
+
+	Resource loadAsResource(String filename);
+
+	String save(@NonNull Long id, MultipartFile file);
+
+}
--- src/main/java/org/teamraccoon/dreamfusion/images/ImageService.java
+++ src/main/java/org/teamraccoon/dreamfusion/images/ImageService.java
@@ -2,6 +2,7 @@ package org.teamraccoon.dreamfusion.images;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,24 +11,34 @@ import java.text.MessageFormat;
 
 import javax.management.RuntimeErrorException;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+import org.teamraccoon.dreamfusion.config.StorageProperties;
+import org.teamraccoon.dreamfusion.generic.IStorageService;
 import org.teamraccoon.dreamfusion.products.Product;
 import org.teamraccoon.dreamfusion.products.ProductNotFoundException;
 import org.teamraccoon.dreamfusion.products.ProductRepository;
 import org.teamraccoon.dreamfusion.utilities.Time;
 
 @Service
-public class ImageService {
+public class ImageService implements IStorageService {
 
     ImageRepository imageRepository;
     ProductRepository productRepository;
     Time time;
+    private final Path rootLocation;
     private final String uploadDir = "src/main/resources/static/images/";
 
-    public ImageService(ImageRepository imageRepository, ProductRepository productRepository, Time time) {
+    public ImageService(ImageRepository imageRepository, ProductRepository productRepository, Time time, StorageProperties properties) {
+        if(properties.getLocation().trim().length() == 0){
+            throw new StorageException("File upload location can not be Empty."); 
+        }
+
+		this.rootLocation = Paths.get(properties.getLocation());
         this.imageRepository = imageRepository;
         this.productRepository = productRepository;
         this.time = time;
@@ -62,6 +73,30 @@ public class ImageService {
 
         return combinedName;
     }
+
+    @Override
+	public Path load(String filename) {
+		return rootLocation.resolve(filename);
+	}
+
+    @Override
+	public Resource loadAsResource(String filename) {
+		try {
+			Path file = load(filename);
+			Resource resource = new UrlResource(file.toUri());
+			if (resource.exists() || resource.isReadable()) {
+				return resource;
+			}
+			else {
+				throw new StorageFileNotFoundException(
+						"Could not read file: " + filename);
+
+			}
+		}
+		catch (MalformedURLException e) {
+			throw new StorageFileNotFoundException("Could not read file: " + filename, e);
+		}
+	}
 
     public Image markImageAsMain(Image image) {
         image.setMainImage(true);
