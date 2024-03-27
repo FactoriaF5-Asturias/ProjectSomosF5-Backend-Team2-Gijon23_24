11ab4e2d28447b40b35540758b844ae70284cc13
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-03-27 12:50:25 +0100

    controller tests are in progress

--- src/main/java/org/teamraccoon/dreamfusion/products/ProductDTO.java
+++ src/main/java/org/teamraccoon/dreamfusion/products/ProductDTO.java
@@ -1,8 +1,12 @@
 package org.teamraccoon.dreamfusion.products;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductDTO {
     public String productName;
     public String productDescription;
