ef0b33c647b67c062c6d5748402a569742978ab4
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-04-05 12:49:32 +0200

    problems with build resolved

--- src/test/java/org/teamraccoon/dreamfusion/DreamfusionApplicationTests.java
+++ src/test/java/org/teamraccoon/dreamfusion/DreamfusionApplicationTests.java
@@ -1,7 +1,5 @@
 package org.teamraccoon.dreamfusion;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
