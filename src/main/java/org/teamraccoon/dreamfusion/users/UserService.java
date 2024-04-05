1977a8b9634a7f2e4abe8d4e7e8a8d82231cf9c8
Juan Munguia <carlosmunguia23145@gmail.com>
2024-04-05 08:52:18 +0200

    not complete user delete

--- src/main/java/org/teamraccoon/dreamfusion/users/UserService.java
+++ src/main/java/org/teamraccoon/dreamfusion/users/UserService.java
@@ -3,20 +3,32 @@ package org.teamraccoon.dreamfusion.users;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.teamraccoon.dreamfusion.profiles.ProfileRepository;
 
 @Service
 public class UserService {
 
     UserRepository repository;
+    ProfileRepository profileRepository;
 
-    public UserService(UserRepository repository) {
+    public UserService(UserRepository repository, ProfileRepository profileRepository) {
         this.repository = repository;
+        this.profileRepository = profileRepository;
     }
 
     public List<User> getAll(){
         List<User> users = repository.findAll();
 
         return users;
+    }
+
+    public User delete(Long id)throws Exception{
+        User userToDelete = repository.findById(id).orElseThrow(() -> new UserNotFoundException("user not found"));
+        repository.deleteById(id);
+
+        // Profile profileToDelete = repository.findByUsername()
+
+        return userToDelete;
     }
 
 }
