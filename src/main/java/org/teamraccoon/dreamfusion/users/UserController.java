c103ccaa7284089e53e39845f439551c5924a0d2
Juan Munguia <carlosmunguia23145@gmail.com>
2024-04-16 09:17:29 +0200

    not complete update password

--- src/main/java/org/teamraccoon/dreamfusion/users/UserController.java
+++ src/main/java/org/teamraccoon/dreamfusion/users/UserController.java
@@ -2,7 +2,11 @@ package org.teamraccoon.dreamfusion.users;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +25,16 @@ public class UserController {
         return service.getAll();
     }
 
+    @PutMapping("/password/{id}")
+    public ResponseEntity<?> updatePassword(@PathVariable Long id, @RequestBody User user, @RequestBody UserDto newPassword) {
+        try {
+            User updatedUser = service.changePassword(id, user.getPassword(), newPassword);
+            return ResponseEntity.ok(updatedUser);
+        } catch (UserNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
+
--- src/main/java/org/teamraccoon/dreamfusion/users/UserService.java
+++ src/main/java/org/teamraccoon/dreamfusion/users/UserService.java
@@ -3,6 +3,7 @@ package org.teamraccoon.dreamfusion.users;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.teamraccoon.dreamfusion.facades.encryptations.EncoderFacade;
 import org.teamraccoon.dreamfusion.profiles.ProfileRepository;
 
 @Service
@@ -10,10 +11,12 @@ public class UserService {
 
     UserRepository repository;
     ProfileRepository profileRepository;
+    EncoderFacade enconde;
 
-    public UserService(UserRepository repository, ProfileRepository profileRepository) {
+    public UserService(UserRepository repository, ProfileRepository profileRepository, EncoderFacade enconde) {
         this.repository = repository;
         this.profileRepository = profileRepository;
+        this.enconde = enconde;
     }
 
     public List<User> getAll(){
@@ -22,6 +25,19 @@ public class UserService {
         return users;
     }
 
+    public User changePassword(Long id, String currentPassword, UserDto newPassword) throws Exception{
+        User user = repository.findById(id).orElseThrow(() -> new UserNotFoundException("user not found"));
+
+        String passwordEncoded = enconde.encode("bcrypt", newPassword.password);
+
+        if (currentPassword == passwordEncoded) {
+            user.setPassword(passwordEncoded);
+        }
+        
+        repository.save(user);
+        return user;
+    }
+    
     public User delete(Long id)throws Exception{
         User userToDelete = repository.findById(id).orElseThrow(() -> new UserNotFoundException("user not found"));
         repository.deleteById(id);
